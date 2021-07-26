0578363abf30bd627e2464ee7ac7a2ff14d5ca62
Yifan Chen <9yifanchen9@gmail.com>
2021-07-26 15:11:28 +1000

    Label mapping between period and frequency

--- EchellePlotter.py
+++ EchellePlotter.py
@@ -1,20 +1,25 @@
 import numpy as np
 import pandas as pd
 from astropy.convolution import convolve, Box1DKernel
-from matplotlib.widgets import Slider, RadioButtons, Button
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, RadioButtons, Button
 
 class EchellePlotter:
     def __init__(self,     
         freq, power,
-        Dnu_min, Dnu_max, fmin=0, fmax=None,
-        step=None, cmap="BuPu", fig=None, ax=None,
-        interpolation=None, smooth=False, smooth_filter_width=50.0,
-        scale=None, return_coords=False, **kwargs):
-
+        Dnu_min, Dnu_max, fmin=0, fmax=None, step=None,
+        plot_period=False, DP_min=None, DP_max=None, pstep=None,
+        cmap="BuPu", interpolation=None, smooth=False, smooth_filter_width=50.0, scale=None):
+    #==================================================================
+    # Class attributes and argument checks
+    #==================================================================
         self.Dnu = (Dnu_min + Dnu_max) / 2.0
         self.fmin = fmin
         self.fmax = fmax
+        self.scale = scale
+
+        self.plot_period = plot_period
+        self.DP = (DP_min + DP_max) / 2.0
 
         # Styles for labels
         self.colors = {0: "blue", 1: "green", 2: "orange"}
@@ -33,42 +38,65 @@ class EchellePlotter:
         self.freq = freq
         self.power = power
 
-        # Compute echelle
-        self.x, self.y, self.z = echelle(freq, self.power, self.Dnu, 
-            sampling=1, fmin=fmin, fmax=fmax)
+    #==================================================================
+    # Data preparation
+    #==================================================================
+        self.update_echelle()
+        if self.plot_period:
+            # Minimum frequency is maximum period
+            self.pmin = self.f2p(self.fmax)
+            self.pmax = self.f2p(self.fmin)
+            self.period = self.f2p(np.array(self.freq))
+            self.update_period_echelle()
 
-        # Scale image intensities
-        if scale is "sqrt":
-            self.z = np.sqrt(self.z)
-        elif scale is "log":
-            self.z = np.log10(self.z)
-        self.scale = scale
-
+    #==================================================================
+    # Plotting
+    #==================================================================
         # Create subplot(s)
-        if ax == None:
-            self.fig, self.ax = plt.subplots()
-        else:
-            self.fig = ax.figure
-            self.ax = ax
+        if self.plot_period:
+            self.fig, self.axs = plt.subplots(1, 2)
+            self.ax = self.axs[0]
+            self.pax = self.axs[1]
 
-        plt.subplots_adjust(left=0.3, bottom=0.25)
+            # Set period y labels to the right
+            self.pax.yaxis.set_label_position("right")
+            self.pax.yaxis.tick_right()
+
+        else:
+            self.fig, self.ax = plt.subplots()
+
+        plt.subplots_adjust(left=0.20, bottom=0.25, wspace=0.05)
 
         # Step in x-axis as median difference
         if step is None:
             step = np.median(np.diff(self.freq))
-
         self.step = step
 
+        # Create widgets
+        self.line = self.create_image(self.ax, self.x, self.y, self.z, 
+            cmap, interpolation,
+            xlabel=u"Frequency mod \u0394\u03BD", ylabel="Frequency (\u03BCHz)")
+        self.create_Dnu_slider(Dnu_min, Dnu_max, step)
+
+        if self.plot_period:
+            self.pline = self.create_image(self.pax, self.px, self.py, self.pz, 
+                cmap, interpolation,
+                xlabel=u"Period mod \u0394P", ylabel="Period (s)")
+            
+            # self.pax.invert_xaxis()
+            # self.pax.invert_yaxis()
+            # self.update_period_echelle()# Update after inverted axes
+
+            self.create_DP_slider(DP_min, DP_max, pstep)
+
+    #==================================================================
+    # Labelling
+    #==================================================================
+        self.create_label_radio_buttons()
+        self.create_remove_label_button()
         # A list of l-mode values (used for removing points)
         self.l_labels = []
-
         self.legend_labels = []
-
-        # Create widgets
-        self.create_Dnu_slider(Dnu_min, Dnu_max, step)
-        self.create_label_radio_buttons()
-        self.create_image(self.x, self.y, self.z, cmap, interpolation)
-        self.create_remove_label_button()
 
         # List of 3 arrays, with index being l-mode label
         # e.g. label_[1][2] is 3rd frequency for l=1 mode label
@@ -78,15 +106,32 @@ class EchellePlotter:
         self.create_label_scatters()
         self.cid = self.fig.canvas.mpl_connect("button_press_event", self.on_click)
 
-    def create_label_scatters(self):
-        """ Scatter plots for l-mode labelling """
-        self.scatters = [None, None, None]
+#======================================================================
+# Echelle diagram functionality
+#======================================================================
+    def create_image(self, ax, x, y, z, cmap, interpolation, xlabel="", ylabel=""):
+        """Create the echelle image"""
+        line = ax.imshow(
+            z,
+            aspect="auto",
+            extent=(x.min(), x.max(), y.min(), y.max()),
+            origin="lower",
+            cmap=cmap,
+            interpolation=interpolation,
+        )
 
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+
+        return line
 
     def create_Dnu_slider(self, Dnu_min, Dnu_max, step):
         """Create Slider that adjusts Dnu"""
         # Frequency adjust axes
-        axfreq = plt.axes([0.25, 0.1, 0.65, 0.03])
+        if self.plot_period:
+            axfreq = plt.axes([0.20, 0.1, 0.25, 0.03])
+        else:
+            axfreq = plt.axes([0.25, 0.1, 0.65, 0.03])
 
         # Value format string
         valfmt = "%1." + str(len(str(step).split(".")[-1])) + "f"
@@ -104,56 +149,55 @@ class EchellePlotter:
         self.fig.canvas.mpl_connect("key_press_event", self.on_key_press)
         self.slider.on_changed(self.update)
 
-    def create_label_radio_buttons(self):
-        """
-        Create RadioButtons to select labelling
-        """
-        # axcolor = 'lightgoldenrodyellow'
-        axcolor = 'white'
-        rax = plt.axes([0.03, 0.7, 0.15, 0.15], facecolor=axcolor)
-        self.radio_l = RadioButtons(rax, ('-', 'l=0', 'l=1', 'l=2'))
+    def create_DP_slider(self, DP_min, DP_max, step):
+        """Create Slider that adjusts Dnu"""
+        # Period adjust axes
+        axperiod = plt.axes([0.60, 0.1, 0.25, 0.03])
 
-    def create_remove_label_button(self):
-        """Create Button to remove last label"""
-        axcolor = 'white'
-        rax = plt.axes([0.03, 0.3, 0.15, 0.10], facecolor=axcolor)
-        self.remove_button = Button(rax, "Undo")
-        self.remove_button.on_clicked(self.remove_previous_label)
+        # Value format string
+        valfmt = "%1." + str(len(str(step).split(".")[-1])) + "f"
 
-    def create_image(self, x, y, z, cmap, interpolation):
-        """Create the echelle image"""
-        self.line = self.ax.imshow(
-            z,
-            aspect="auto",
-            extent=(x.min(), x.max(), y.min(), y.max()),
-            origin="lower",
-            cmap=cmap,
-            interpolation=interpolation,
+        # Slider and event calls
+        self.pslider = Slider(
+            axperiod,
+            u"\u0394P",
+            DP_min,
+            DP_max,
+            valinit=(DP_min + DP_max) / 2.0,
+            valstep=step,
+            valfmt=valfmt,
         )
+        self.fig.canvas.mpl_connect("key_press_event", self.on_key_press)
+        self.pslider.on_changed(self.pupdate)
 
-        self.ax.set_xlabel(u"Frequency mod \u0394\u03BD")
-        self.ax.set_ylabel("Frequency")
+    def update_echelle(self):
+        """Get new period echelle"""
+        self.x, self.y, self.z = echelle(self.freq, self.power, self.Dnu, 
+            sampling=1, fmin=self.fmin, fmax=self.fmax)
+        # Scale image intensities
+        if self.scale is "sqrt":
+            self.z = np.sqrt(self.z)
+        elif self.scale is "log":
+            self.z = np.log10(self.z)
 
-    def show(self):
-        """Show plot using plt.show()"""
-        plt.show()
+    def update_period_echelle(self):
+        """Get new period echelle"""
+        # Descending period to match frequency plot
+        self.px, self.py, self.pz = echelle(self.period[::-1], self.power[::-1], 
+            self.DP, sampling=1, fmin=self.pmin, fmax=self.pmax)
+        # Scale image intensities
+        if self.scale is "sqrt":
+            self.pz = np.sqrt(self.pz)
+        elif self.scale is "log":
+            self.pz = np.log10(self.pz)
 
     def update(self, Dnu):
-        """Updates echelle diagram given new Dnu"""
+        """Updates frequency echelle diagram given new Dnu"""
         self.Dnu = Dnu
-
-        # Calculate new data
-        self.x, self.y, self.z = echelle(self.freq, self.power, self.Dnu, sampling=1, fmin=self.fmin, fmax=self.fmax)
-        if self.scale != None:
-            if self.scale == "sqrt":
-                self.z = np.sqrt(self.z)
-            elif self.scale == "log":
-                self.z = np.log10(self.z)
+        self.update_echelle()
         self.line.set_array(self.z)
 
-        # Set new visible range for x and y axes
-        self.line.set_extent((self.x.min(), self.x.max(), self.y.min(), self.y.max()))
-        self.ax.set_xlim(0, self.Dnu)
+        self.set_extent()
 
         # Shift labelled points accordingly
         self.update_labels()
@@ -161,25 +205,69 @@ class EchellePlotter:
         # Render
         self.fig.canvas.blit(self.ax.bbox)
 
-    def update_labels(self):
-        """Update the labels to new echelle diagram coordinates"""
-        # Replot
-        l = 0
-        while l < len(self.f_labels):
-            self.update_scatter(l)
-            l += 1
+    def pupdate(self, DP):
+        """Updates period echelle diagram given new DP"""
+        self.DP = DP
+
+        # Calculate new data
+        self.update_period_echelle()
+        self.pline.set_array(self.pz)
+
+        # Set new visible range for x and y axes (reverted)
+        # self.pline.set_extent((self.px.max(), self.px.min(), self.py.max(), self.py.min()))
+        # self.pax.set_xlim(self.DP, 0)
+
+        self.set_pextent()
+
+        # Shift labelled points accordingly
+        self.update_labels()
+
+        # Render
+        self.fig.canvas.blit(self.pax.bbox)
 
     def on_key_press(self, event):
         """Key press to shift slider left and right"""
-        if event.key == "left":
-            new_Dnu = self.slider.val - self.slider.valstep
-        elif event.key == "right":
-            new_Dnu = self.slider.val + self.slider.valstep
-        else:
-            new_Dnu = self.slider.val
+        key = event.key.lower()
+        if key == "left" or key == "right":
+            if key == "left":
+                new_Dnu = self.slider.val - self.slider.valstep
+            else:
+                new_Dnu = self.slider.val + self.slider.valstep
+            self.slider.set_val(new_Dnu)
+            self.update(new_Dnu)
 
-        self.slider.set_val(new_Dnu)
-        self.update(new_Dnu)
+        elif self.plot_period and (key == "h" or key == "l"):
+            if key == "h":
+                new_DP = self.pslider.val - self.pslider.valstep
+            elif key == "l":
+                new_DP = self.pslider.val + self.pslider.valstep
+
+            self.pslider.set_val(new_DP)
+            self.pupdate(new_DP)
+
+#======================================================================
+# Point Labelling
+#======================================================================
+    def create_label_scatters(self):
+        """ Scatter plots for l-mode labelling """
+        self.scatters = [None, None, None]
+        self.pscatters = [None, None, None]
+
+    def create_label_radio_buttons(self):
+        """
+        Create RadioButtons to select labelling
+        """
+        # axcolor = 'lightgoldenrodyellow'
+        axcolor = 'white'
+        rax = plt.axes([0.02, 0.7, 0.08, 0.15], facecolor=axcolor)
+        self.radio_l = RadioButtons(rax, ('-', 'l=0', 'l=1', 'l=2'))
+
+    def create_remove_label_button(self):
+        """Create Button to remove last label"""
+        axcolor = 'white'
+        rax = plt.axes([0.02, 0.3, 0.08, 0.10], facecolor=axcolor)
+        self.remove_button = Button(rax, "Undo")
+        self.remove_button.on_clicked(self.remove_previous_label)
 
     def on_click(self, event):
         """Clicking event"""
@@ -196,7 +284,6 @@ class EchellePlotter:
         
         y_inc = self.y[1] - self.y[0]
 
-
         # Find the nearest x and y value in self.x and self.y
         nearest_x_index = (np.abs(self.x-x)).argmin()
 
@@ -207,7 +294,7 @@ class EchellePlotter:
 
     def add_point(self, x, y, l):
         """Add point to the plot and update scatter"""
-        f = self.coord_to_freq(x, y)
+        f = self.coord2freq(x, y)
         self.f_labels[l].append(f)
         self.l_labels.append(l)
         self.update_scatter(l)
@@ -220,6 +307,24 @@ class EchellePlotter:
         l = self.l_labels.pop()
         self.f_labels[l].pop()
         self.update_scatter(l)
+
+    def set_extent(self):
+        """Set new visible range for x and y axes of frequency echelle"""
+        self.line.set_extent((self.x.min(), self.x.max(), self.y.min(), self.y.max()))
+        self.ax.set_xlim(0, self.Dnu)
+
+    def set_pextent(self):
+        """Set new visible range for x and y axes of period echelle"""
+        self.pline.set_extent((self.px.min(), self.px.max(), self.py.min(), self.py.max()))
+        self.pax.set_xlim(0, self.DP)        
+
+    def update_labels(self):
+        """Update the labels to new echelle diagram coordinates"""
+        # Replot
+        l = 0
+        while l < len(self.f_labels):
+            self.update_scatter(l)
+            l += 1
 
     def update_scatter(self, l):
         """Updates scatter plot"""
@@ -234,6 +339,9 @@ class EchellePlotter:
         elif no_label_has_scatter:
             self.scatters[l].remove()
             self.scatters[l] = None
+            self.pscatters[l].remove()
+            self.pscatters[l] = None
+
             self.legend_labels.remove(f"l={l}")
 
         # First label of the mode
@@ -243,35 +351,37 @@ class EchellePlotter:
             marker = self.markers[l]
             label = f"l={l}"
 
+            # Get frequency coordinates
             x_labels, y_labels = self.get_coords(l, self.Dnu)
-
             self.scatters[l] = self.ax.scatter(x_labels, y_labels, 
                 s=50, marker=marker, label=label, color=color)
-            
-            # Set new visible range for x and y axes
-            self.line.set_extent((self.x.min(), self.x.max(), self.y.min(), self.y.max()))
-            self.ax.set_xlim(0, Dnu)            
 
-            label = f"l={l}"
+            # Period coordinates
+            px_labels, py_labels = self.get_pcoords(l, self.DP)
+            self.pscatters[l] = self.pax.scatter(px_labels, py_labels, 
+                s=50, marker=marker, label=label, color=color)
+
+            # Legend labels
             if label not in self.legend_labels:
                 self.legend_labels.append(f"l={l}")
         else:
             x_labels, y_labels = self.get_coords(l, self.Dnu)
             self.scatters[l].set_offsets(np.c_[x_labels, y_labels])
         
+            px_labels, py_labels = self.get_pcoords(l, self.DP)
+            self.pscatters[l].set_offsets(np.c_[px_labels, py_labels])
+
+        # Prevent view from changing after point is added
+        self.set_extent()
+        self.set_pextent()
         self.ax.legend(self.legend_labels)
         self.fig.canvas.draw()
 
+    def get_legend_labels(self):
+        """Get legend labels based on whether there is data for each legend"""
+        return [f"l={l}" for l in range(len(self.f_labels)) if len(self.f_labels[l]) != 0]
 
-    def freq_to_period(self, freq):
-        """From frequency (muHz) to period (s)"""
-        return 1e6/freq
-
-    def period_to_freq(self, period):
-        """From period (s) to frequency (muHz)"""
-        return 1e6/period
-
-    def period_to_coord(self, period, DP):
+    def p2coord(self, period, DP):
         """Period to coordinate on the Echelle period"""
         y_inc = self.y[1] - self.y[0]
         x = (freq - self.x.min()) % Dnu# freq mod Dnu is x-coordinate
@@ -282,26 +392,56 @@ class EchellePlotter:
         xs = []
         ys = []
         for f in self.f_labels[l]:
-            x, y = self.freq_to_coord(f, Dnu)
+            x, y = self.f2coord(f, Dnu)
+            xs.append(x)
+            ys.append(y)
+
+        return xs, ys
+    
+    def get_pcoords(self, l, DP):
+        xs = []
+        ys = []
+        for f in self.f_labels[l]:
+            p = self.p2f(f)
+            x, y = self.p2coord(p, DP)
             xs.append(x)
             ys.append(y)
 
         return xs, ys
 
-    def freq_to_coord(self, freq, Dnu):
+    def f2coord(self, freq, Dnu):
         """Frequency to coordinate on the Echelle"""
         y_inc = self.y[1] - self.y[0]
         x = (freq - self.x.min()) % Dnu# freq mod Dnu is x-coordinate
         # The bin just below the frequency, plus half increment (for labelling in middle) 
-        y = self.y[self.y < freq].max() + 0.5*(self.y[1] - self.y[0])
+        y = self.y[self.y < freq].max() + 0.5*y_inc
         return x, y
 
-    def coord_to_freq(self, x, y):
+    def p2coord(self, period, DP):
+        """Period to coordinate on the Echelle"""
+        py_inc = self.py[1] - self.py[0]
+        x = (period - self.px.min()) % DP# period mod DP is x-coordinate
+        # The bin just below the frequency, plus half increment (for labelling in middle)
+        y = self.py[self.py < period].max() + 0.5*py_inc
+        return x, y
+
+    def coord2freq(self, x, y):
         return y + x
 
-    def get_legend_labels(self):
-        """Get legend labels based on whether there is data for each legend"""
-        return [f"l={l}" for l in range(len(self.f_labels)) if len(self.f_labels[l]) != 0]
+#======================================================================
+# Utilities
+#======================================================================
+    def show(self):
+        """Show plot using plt.show()"""
+        plt.show()
+
+    def f2p(self, freq):
+        """From frequency (muHz) to period (s)"""
+        return 1e6/freq
+
+    def p2f(self, period):
+        """From period (s) to frequency (muHz)"""
+        return 1e6/period
 
 
 def echelle(freq, power, Dnu, fmin=0.0, fmax=None, offset=0.0, sampling=0.1):
@@ -410,9 +550,16 @@ if __name__ == "__main__":
     
 
     Dnu = 8.10 # large frequency separation (muHz)
-    fmin = 102 - 40
-    fmax = 102 + 40
+    fmin = 102 - 25
+    fmax = 102 + 25
 
-    e = EchellePlotter(freq, amp, Dnu-3, Dnu+3, step=.05, 
-        fmin=fmin, fmax=fmax, return_coords=True)
+    # Change to period
+    DP = 194.0 # period spacing
+
+    e = EchellePlotter(freq, amp, Dnu_min=Dnu-3, Dnu_max=Dnu+3, step=.05,
+        fmin=fmin, fmax=fmax, plot_period=True, 
+        DP_min=DP-10, DP_max=DP+10, pstep=0.1)
     e.show()
+
+
+
